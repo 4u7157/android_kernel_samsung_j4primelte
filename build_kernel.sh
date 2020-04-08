a6eb6f7e11049083e836eb0c1e9bfeff1e3f8e9e
kapmino269-MG <kapmino269@gmail.com>
2020-04-08 11:18:41 -0400

    Add stuff for kali hunter

--- build_kernel.sh
+++ build_kernel.sh
@@ -4,8 +4,9 @@ COMMON_ARGS="-C $(pwd) O=$(pwd)/${OUT_DIR} ARCH=arm CROSS_COMPILE=arm-linux-andr
 export PATH=/home/kapmino269/arm-linux-androideabi-4.9-brillo-m7-dev/bin:$PATH export ARCH=arm 
 [ -d ${OUT_DIR} ] && rm -rf ${OUT_DIR} 
 mkdir ${OUT_DIR} 
+make clean
 make clean ${OUT_DIR} && make mrproper ${OUT_DIR} 
 make ${COMMON_ARGS} j4primelte_sea_open_defconfig 
-//make ${COMMON_ARGS} menuconfig 
-make -j$(nproc --all) ${COMMON_ARGS} 
-cp ${OUT_DIR}/arch/arm/boot/zImage $(pwd)/arch/arm/boot/zImage
+make ${COMMON_ARGS} nconfig 
+#make -j$(nproc --all) ${COMMON_ARGS} 
+#cp ${OUT_DIR}/arch/arm/boot/zImage $(pwd)/arch/arm/boot/zImage
